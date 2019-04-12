e94803476bf2ae14a9832d8232f6d41393c03e42
Lucien Catonnet <lutopia@hotmail.fr>
2019-04-12 22:34:44 +0200

    Support years as time unit

--- src/js/util.js
+++ src/js/util.js
@@ -1,6 +1,7 @@
 function time_unit(u) {
   var seconds = 1;
   switch(u) {
+    case 'y': return seconds * 31557600;
     case 'w': seconds *= 7;
     case 'd': seconds *= 24;
     case 'h': seconds *= 60;
