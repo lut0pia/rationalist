b3fed9ca74a4718988ff9782035a5cb659992132
Lucien Catonnet <lutopia@hotmail.fr>
2019-06-29 13:14:17 +0200

    Added version to storage to allow wipes

--- src/js/storage.js
+++ src/js/storage.js
@@ -1,9 +1,18 @@
 // Local storage helper
+const current_storage_version = 1;
+
 try {
   storage = JSON.parse(localStorage.json);
 } catch(e) {
   storage = {};
 }
+
+if(!storage.version || storage.version < current_storage_version) {
+  storage = {
+    version: current_storage_version,
+  };
+}
+
 window.addEventListener('unload', function() {
   try {
     localStorage.json = JSON.stringify(storage);
